80b78ad42843d70341d1f3f408f886a16fb4c779
GALLA PRUDHVI RAJ <pruney1627@gmail.com>
2026-04-05 00:51:04 +0530

    added new variables

---
+++ script.js
@@ -0,0 +1,8 @@
+const date = document.getElementById("date");
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+
+const today = new Data();
+
+console.log(today);
