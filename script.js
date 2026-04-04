b1c9d0b71e348eef10b39cfe7f354e91a511f05d
GALLA PRUDHVI RAJ <pruney1627@gmail.com>
2026-04-05 00:54:18 +0530

    made changes to show date, month and year

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const day = document.getElementById("day");
 const month = document.getElementById("month");
 const year = document.getElementById("year");
 
-const today = new Data();
+const today = new Date();
 
 console.log(today);
+
+date.innerHTML = today.getDate();
+day.innerHTML = today.getDay();
+month.innerHTML = today.getMonth();
+year.innerHTML = today.getFullYear();
